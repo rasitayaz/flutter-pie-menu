9700afc5519c63f82ed99735d46d0e908834cfbb
Batmend Ganbaatar <batmend@gege.mn>
2024-02-16 19:42:30 +0800

    chore: add [alwaysPlaceActionFromCenter]

--- lib/src/pie_theme.dart
+++ lib/src/pie_theme.dart
@@ -48,6 +48,7 @@ class PieTheme {
     this.angleOffset = 0,
     this.customAngle,
     this.customAngleAnchor = PieAnchor.center,
+    this.alwaysPlaceActionFromCenter = true,
     this.buttonSize = 56,
     this.pointerSize = 40,
     this.tooltipPadding = const EdgeInsets.all(32),
@@ -115,6 +116,13 @@ class PieTheme {
 
   /// Action display alignment for the specified [customAngle].
   final PieAnchor customAngleAnchor;
+
+  /// If set to true, all actions will be rendered from the center
+  /// of the widget.
+  ///
+  /// This is ideal if you're using [PieMenu] with small icon buttons
+  /// or [FloatingActionButton]s
+  final bool alwaysPlaceActionFromCenter;
 
   /// Size of [PieButton] circle.
   final double buttonSize;
