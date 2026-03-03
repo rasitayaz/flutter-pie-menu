017e28faf1b622deac210bb914aa854525039788
Jonathan Lau <jon@jon.sg>
2026-03-03 15:57:31 +0800

    Bump sdk version to 3.8.0

--- lib/src/pie_button.dart
+++ lib/src/pie_button.dart
@@ -106,16 +106,14 @@ class _PieButtonState extends State<PieButton>
               AnimatedPositioned(
                 duration: _theme.hoverDuration,
                 curve: Curves.ease,
-                top:
-                    widget.hovered
-                        ? _theme.buttonSize / 2 -
-                            sin(widget.angle) * _theme.hoverDisplacement
-                        : _theme.buttonSize / 2,
-                right:
-                    widget.hovered
-                        ? _theme.buttonSize / 2 -
-                            cos(widget.angle) * _theme.hoverDisplacement
-                        : _theme.buttonSize / 2,
+                top: widget.hovered
+                    ? _theme.buttonSize / 2 -
+                          sin(widget.angle) * _theme.hoverDisplacement
+                    : _theme.buttonSize / 2,
+                right: widget.hovered
+                    ? _theme.buttonSize / 2 -
+                          cos(widget.angle) * _theme.hoverDisplacement
+                    : _theme.buttonSize / 2,
                 child: Container(
                   height: _theme.buttonSize,
                   width: _theme.buttonSize,
@@ -125,18 +123,16 @@ class _PieButtonState extends State<PieButton>
                           : _buttonTheme.decoration) ??
                       BoxDecoration(
                         shape: BoxShape.circle,
-                        color:
-                            widget.hovered
-                                ? _buttonThemeHovered.backgroundColor
-                                : _buttonTheme.backgroundColor,
+                        color: widget.hovered
+                            ? _buttonThemeHovered.backgroundColor
+                            : _buttonTheme.backgroundColor,
                       ),
                   child: Center(
                     child: IconTheme(
                       data: IconThemeData(
-                        color:
-                            widget.hovered
-                                ? _buttonThemeHovered.iconColor
-                                : _buttonTheme.iconColor,
+                        color: widget.hovered
+                            ? _buttonThemeHovered.iconColor
+                            : _buttonTheme.iconColor,
                         size: _theme.iconSize,
                       ),
                       child:
