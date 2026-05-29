98b83f388e29cbaf4019f2c00309cad2bb80f85a
Jonathan Lau <me@jon.sg>
2026-05-29 14:36:04 +0800

    Respect parent icon theme by using IconTheme.merge

--- test/pie_button_test.dart
+++ test/pie_button_test.dart
@@ -69,5 +69,51 @@ void main() {
       final decoration = container.decoration as BoxDecoration;
       expect(decoration.color, Colors.red);
     });
+
+    testWidgets('overrides color and size from parent icon theme and respects '
+        'other properties', (tester) async {
+      final action = PieAction(
+        tooltip: const Text('Tooltip'),
+        onSelect: () {},
+        child: const Icon(Icons.add),
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: IconTheme(
+            data: const IconThemeData(
+              color: Colors.red,
+              size: 36.0,
+              weight: 700,
+            ),
+            child: PieCanvas(
+              child: Scaffold(
+                body: PieButton(
+                  theme: const PieTheme(
+                    iconSize: 24.0,
+                    buttonTheme: PieButtonTheme(
+                      backgroundColor: Colors.blue,
+                      iconColor: Colors.green,
+                    ),
+                  ),
+                  action: action,
+                  angle: 0,
+                  hovered: false,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byIcon(Icons.add), findsOneWidget);
+
+      final iconThemeData = IconTheme.of(
+        tester.element(find.byIcon(Icons.add)),
+      );
+      expect(iconThemeData.color, Colors.green);
+      expect(iconThemeData.size, 24.0);
+      expect(iconThemeData.weight, 700);
+    });
   });
 }
